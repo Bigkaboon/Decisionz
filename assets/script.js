3e1591278e0b8af0f69d6867067861316f3b433b
Bigkaboon <>
2023-10-15 10:56:47 +0000

    added the functions I plan to use to script.js

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementById("button");
+
+    
+
+});
+
+function checkQuestionType() {
+
+}
+
+function generateAnswer() {
+
+}
+
+function yesOrNo() {
+
+}
+function shouldOrShouldNot() {
+
+}
+function alternatives() {
+
+}
+function scale() {
+
+}
+
+

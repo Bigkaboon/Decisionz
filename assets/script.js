e6736abd9fb94257466671a3ac88e852e4cafa3c
Bigkaboon <>
2023-10-19 17:22:46 +0000

    created email submit form and installed @emailjs/browser

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 document.addEventListener("DOMContentLoaded", function () {
+    
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
@@ -206,6 +210,29 @@ document.getElementById('alternative-button').addEventListener('click', function
     const holder = document.getElementById('alternative-inputs');
     holder.innerHTML = '';
 
-
-
 });
+const contact = contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+    <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+    </form>;
+};
+
